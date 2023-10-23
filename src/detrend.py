0695619f7151a6f59718699edf32f68c30cb4ff5
jExbrayat <jules.exbrayat@gmail.com>
2023-10-23 18:52:17 +0200

    feat: create ExponentialMA class

--- src/detrend.py
+++ src/detrend.py
@@ -186,3 +186,44 @@ class LinearMA:
             xticklabels=xticklabels,
             method_name=self.method_name,
         )
+
+
+class ExponentialMA:
+    def __init__(self, alpha: float = 0.05) -> None:
+        self.fitted_parameters = {"Alpha": alpha}
+        self.alpha = alpha
+        self.method_name = "exponential mobile average"
+
+    def fit(self, y: np.ndarray | pd.DataFrame) -> np.ndarray:
+        """
+        returns fitted values with the exponential mobile average method
+        """
+        expo_MA = pd.DataFrame(y).ewm(alpha=self.alpha, adjust=False).mean()
+
+        self.y_original = y
+        self.fitted_values = np.array(expo_MA).ravel()
+
+    def predict(self) -> np.ndarray:
+        """_summary_
+
+        Returns:
+            np.ndarray: detrended values, 1 dimensional array of length len(y)
+        """
+        self.y_predict = self.y_original - self.fitted_values
+        return self.y_predict
+
+    def fancy_plot(self, xticklabels: pd.core.indexes.base.Index | None = None) -> None:
+        """plot two graphs : the original data and its fitted trend curve ; the detrended data
+
+        Args:
+            xticklabels (pd.core.indexes.base.Index | None, optional): the date index of the imported
+            financial data. Defaults to None.
+        """
+        _fancy_plot(
+            y_original=self.y_original,
+            y_fitted=self.fitted_values,
+            y_detrend=self.y_predict,
+            fitted_parameters=self.fitted_parameters,
+            xticklabels=xticklabels,
+            method_name=self.method_name,
+        )
