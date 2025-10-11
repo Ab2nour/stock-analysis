e688b137cc8a86939770378c429f52acc9fa02c4
Ab2nour <61651582+Ab2nour@users.noreply.github.com>
2025-10-11 17:11:32 +0200

    refactor: load_data file

--- src/data/load_data.py
+++ src/data/load_data.py
@@ -4,10 +4,21 @@ import pandas as pd
 from src.utils.constants import data_folder
 
 
+def load_stock_data(stock_name: str) -> pd.DataFrame:
+    """Load stock data.
+
+    >>> stock_data = load_stock_data("AAPL")
+    """
+    stock_data = pd.read_csv(
+        f"{data_folder}/raw/{stock_name}.csv", parse_dates=["Date"], index_col="Date"
+    )
+    return stock_data
+
+
 def load_sample_data() -> pd.DataFrame:
     """Load sample data.
 
     >>> sample_data = load_sample_data()
     """
-    sample_data = pd.read_csv(f"{data_folder}/raw/AAPL.csv")
+    sample_data = load_stock_data("AAPL")
     return sample_data
