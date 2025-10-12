03cb5c7c6be0abdd0fa3dddbf8047a274fba198c
Ab2nour <61651582+Ab2nour@users.noreply.github.com>
2025-10-12 16:00:17 +0200

    refactor: create machine_learning_detrend.py

---
+++ src/regression/models/machine_learning_detrend.py
@@ -0,0 +1,21 @@
+"""Machine learning models on de-trended data."""
+
+from typing import TypeAlias
+
+import numpy as np
+from numpy.typing import ArrayLike
+from sklearn.ensemble import ExtraTreesRegressor
+from sklearn.linear_model import LinearRegression
+from xgboost import XGBRegressor
+
+MLRegressor: TypeAlias = XGBRegressor | ExtraTreesRegressor
+DetrendRegressor: TypeAlias = LinearRegression
+
+
+def detrend_prediction(
+    model: MLRegressor, regressor: LinearRegression, x: ArrayLike
+) -> np.ndarray:
+    lr_predict = regressor.predict(x).reshape(-1, 1)
+    y_pred = model.predict(x).reshape(-1, 1)
+
+    return y_pred + lr_predict
